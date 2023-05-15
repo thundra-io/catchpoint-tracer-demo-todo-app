1e44718e545e6770d74414af61b0503cc60e4a3d
mehmetyz <mehmetyildiz497@icloud.com>
2023-05-15 17:33:12 +0300

    fix stage options for app name

--- deployment/app/bin/app.ts
+++ deployment/app/bin/app.ts
@@ -4,6 +4,6 @@ import * as cdk from 'aws-cdk-lib';
 import { DeployStack } from '../lib/deploy-stack';
 
 const app = new cdk.App();
-new DeployStack(app, 'cp-tracing-demo-todo-app-deploy-stack');
+new DeployStack(app, 'cp-tracing-demo-todo-app-deploy-stack-' + (process.env.PROFILE || 'lab'), {});
 
 app.synth();
--- deployment/app/lib/deploy-stack.ts
+++ deployment/app/lib/deploy-stack.ts
@@ -6,45 +6,50 @@ import { Construct } from 'constructs';
 
 import * as path from 'path';
 
-const APP_NAME = `cp-tracing-demo-todo-app-${process.env.PROFILE || 'lab'}`;
+const APP_NAME = `cp-tracing-demo-todo-app`;
+const PROFILE = process.env.PROFILE || 'lab';
 
 export class DeployStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const role = new iam.Role(this, `${APP_NAME}-role`, {
+    const role = new iam.Role(this, `${APP_NAME}-role-${PROFILE}`, {
       assumedBy: new iam.ServicePrincipal('ec2.amazonaws.com'),
     });
 
     const managedPolicy = iam.ManagedPolicy.fromAwsManagedPolicyName('AWSElasticBeanstalkWebTier');
     role.addManagedPolicy(managedPolicy);
 
-    const instanceProfile = new iam.CfnInstanceProfile(this, `${APP_NAME}-instance-profile`, {
+    const instanceProfile = new iam.CfnInstanceProfile(this, `${APP_NAME}-instance-profile-${PROFILE}`, {
       roles: [role.roleName],
-      instanceProfileName: `${APP_NAME}-instance-profile`,
+      instanceProfileName: `${APP_NAME}-instance-profile-${PROFILE}`,
     });
 
-    const application = new elasticbeanstalk.CfnApplication(this, APP_NAME, {
-      applicationName: APP_NAME,
+    const application = new elasticbeanstalk.CfnApplication(this, `${APP_NAME}-${PROFILE}`, {
+      applicationName: `${APP_NAME}-${PROFILE}`,
     });
 
-    const appZip = new s3assets.Asset(this, `${APP_NAME}-zipped`, {
+    const appZip = new s3assets.Asset(this, `${APP_NAME}-${PROFILE}-zipped`, {
       path: path.join(__dirname, '../../../target/todo-app.zip'),
     });
 
-    const version = new elasticbeanstalk.CfnApplicationVersion(this, `${APP_NAME}-version`, {
-      applicationName: application.applicationName || APP_NAME,
-      sourceBundle: {
-        s3Bucket: appZip.s3BucketName,
-        s3Key: appZip.s3ObjectKey,
-      },
-    });
+    const version = new elasticbeanstalk.CfnApplicationVersion(
+      this,
+      `${APP_NAME}-${PROFILE}-version`,
+      {
+        applicationName: application.applicationName || `${APP_NAME}-${PROFILE}`,
+        sourceBundle: {
+          s3Bucket: appZip.s3BucketName,
+          s3Key: appZip.s3ObjectKey,
+        },
+      }
+    );
 
     const options: elasticbeanstalk.CfnEnvironment.OptionSettingProperty[] = [
       {
         namespace: 'aws:autoscaling:launchconfiguration',
         optionName: 'IamInstanceProfile',
-        value: instanceProfile.instanceProfileName || `${APP_NAME}-instance-profile`,
+        value: instanceProfile.instanceProfileName || `${APP_NAME}-instance-profile-${PROFILE}`,
       },
       {
         namespace: 'aws:autoscaling:launchconfiguration',
@@ -58,19 +63,19 @@ export class DeployStack extends cdk.Stack {
       },
     ];
 
-    const environment = new elasticbeanstalk.CfnEnvironment(this, `${APP_NAME}-env`, {
-      environmentName: `${APP_NAME}-env`,
-      applicationName: application.applicationName || APP_NAME,
+    const environment = new elasticbeanstalk.CfnEnvironment(this, `${APP_NAME}-${PROFILE}-env`, {
+      environmentName: `${APP_NAME}-${PROFILE}-env`,
+      applicationName: application.applicationName || `${APP_NAME}-${PROFILE}`,
       solutionStackName: '64bit Amazon Linux 2 v3.4.7 running Corretto 8',
       optionSettings: options,
       versionLabel: version.ref,
-      cnamePrefix: `${APP_NAME}`,
+      cnamePrefix: `${APP_NAME}-${PROFILE}`,
     });
 
     version.addDependency(application);
     environment.addDependency(version);
 
-    new cdk.CfnOutput(this, `${APP_NAME}-url`, {
+    new cdk.CfnOutput(this, `${APP_NAME}-${PROFILE}-url`, {
       value: environment.attrEndpointUrl,
     });
   }
