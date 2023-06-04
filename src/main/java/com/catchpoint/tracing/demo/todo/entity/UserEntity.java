f054ee561fa386a07036049394d7dfdbc4bec767
Serkan ÖZAL <sozal@catchpoint.com>
2023-06-04 09:38:35 +0300

    Fix checkstyle issue

--- src/main/java/com/catchpoint/tracing/demo/todo/entity/UserEntity.java
+++ src/main/java/com/catchpoint/tracing/demo/todo/entity/UserEntity.java
@@ -1,8 +1,6 @@
 package com.catchpoint.tracing.demo.todo.entity;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
