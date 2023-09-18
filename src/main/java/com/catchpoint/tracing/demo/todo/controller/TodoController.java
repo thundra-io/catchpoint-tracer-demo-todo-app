591016c1aa1257a443306bf31cc57dda989201c5
mehmetyz <mehmetyildiz497@icloud.com>
2023-09-18 11:25:26 +0300

    make checkUser method public

--- src/main/java/com/catchpoint/tracing/demo/todo/controller/TodoController.java
+++ src/main/java/com/catchpoint/tracing/demo/todo/controller/TodoController.java
@@ -64,7 +64,7 @@ public class TodoController {
         this.userClient = userCheckEnabled ? userClient : null;
     }
 
-    private void checkUserAccess() {
+    public void checkUserAccess() {
         if (userClient != null) {
             String userEmail = USER_EMAIL_LIST.get(RANDOM.nextInt(USER_EMAIL_LIST.size()));
             InvocationAPI.setTag("email", userEmail);
