a1438f5bd7306f3c3aea2e6b995d7ff6c9ab1d83
Serkan ÖZAL <sozal@catchpoint.com>
2023-09-25 20:14:18 +0300

    Add SQS integration

--- src/main/java/com/catchpoint/tracing/demo/todo/lambda/NotificationMessageHandler.java
+++ src/main/java/com/catchpoint/tracing/demo/todo/lambda/NotificationMessageHandler.java
@@ -1,22 +1,65 @@
 package com.catchpoint.tracing.demo.todo.lambda;
 
 import com.amazonaws.services.lambda.runtime.Context;
-import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.SQSEvent;
+import io.opentelemetry.instrumentation.awslambdacore.v1_0.TracingRequestHandler;
+import io.opentelemetry.sdk.autoconfigure.AutoConfiguredOpenTelemetrySdk;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author sozal
  */
-public class NotificationMessageHandler implements RequestHandler<SQSEvent, Void> {
+public class NotificationMessageHandler extends TracingRequestHandler<SQSEvent, Void> {
+
+    private static final String TRACE_PARENT = "traceparent";
+    private static final String TRACE_STATE = "tracestate";
+    private static final List<String> HEADER_NAMES =
+            Collections.unmodifiableList(Arrays.asList(TRACE_PARENT, TRACE_STATE));
+
+    public NotificationMessageHandler() {
+        super(AutoConfiguredOpenTelemetrySdk.initialize().getOpenTelemetrySdk(), Duration.ofSeconds(3));
+    }
 
     @Override
-    public Void handleRequest(SQSEvent sqsEvent, Context context) {
-        context.getLogger().log("Env vars: " + System.getenv());
-        for (SQSEvent.SQSMessage message : sqsEvent.getRecords()) {
+    protected Void doHandleRequest(SQSEvent event, Context context) {
+        for (SQSEvent.SQSMessage message : event.getRecords()) {
             context.getLogger().log("Handling notification message: " + message.getBody());
+            context.getLogger().log("Message attributes: " + message.getMessageAttributes());
         }
-
         return null;
     }
 
+    @Override
+    protected Map<String, String> extractHttpHeaders(SQSEvent input) {
+        List<SQSEvent.SQSMessage> messages = input.getRecords();
+        if (!messages.isEmpty()) {
+            SQSEvent.SQSMessage message = messages.get(0);
+            Map<String, SQSEvent.MessageAttribute> messageAttributes = message.getMessageAttributes();
+            Map<String, String> headers = new HashMap<>();
+            if (messageAttributes != null) {
+                for (String headerName : HEADER_NAMES) {
+                    SQSEvent.MessageAttribute messageAttribute = messageAttributes.get(headerName);
+                    if (messageAttribute != null) {
+                        String messageAttributeValue = messageAttribute.getStringValue();
+                        if (messageAttributeValue != null) {
+                            headers.put(headerName, messageAttributeValue);
+                        }
+                    }
+                }
+            }
+            if (!headers.isEmpty()) {
+                return headers;
+            }
+        }
+        return super.extractHttpHeaders(input);
+    }
+
 }
+
+
