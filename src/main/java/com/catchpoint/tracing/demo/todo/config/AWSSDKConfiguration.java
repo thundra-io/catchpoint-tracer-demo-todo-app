94a1852dd6a5d2fff0b51218f7d378405692e990
Serkan ÖZAL <sozal@catchpoint.com>
2023-10-03 12:10:05 +0300

    Ignore AWS SDK config in the `test` profile

--- src/main/java/com/catchpoint/tracing/demo/todo/config/AWSSDKConfiguration.java
+++ src/main/java/com/catchpoint/tracing/demo/todo/config/AWSSDKConfiguration.java
@@ -2,11 +2,13 @@ package com.catchpoint.tracing.demo.todo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import software.amazon.awssdk.services.sqs.SqsClient;
 
 /**
  * @author sozal
  */
+@Profile("!test")
 @Configuration
 public class AWSSDKConfiguration {
 
